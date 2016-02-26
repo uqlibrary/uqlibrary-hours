66cd6343ae0ecafb282b6f7da4f0c6e41b989966
Jan-Willem Wisgerhof <jwwisgerhof@gmail.com>
2016-02-26 13:58:17 +1000

    again

--- bin/codeship-test.sh
+++ bin/codeship-test.sh
@@ -3,30 +3,8 @@
 echo "Deploying branch: ${CI_BRANCH}"
 
 # REDO WHEN POLYMER 1.0 GOES LIVE
-npm install -g web-component-tester@v3.1.7
-
-# If polymer 1.0
-if [ ${CI_BRANCH} == "polymer1.0" ]; then
-
-    if [ ${PIPE_NUM} == 1 ]; then
-        # Run local tests
-        echo "Bower Install"
-        bower install
-        echo "Running tests"
-        wct --verbose
-    else
-        echo "Pipe not used"
-    fi
-else
-    # make sure wct-sauce plugin works with old version of wct
-    npm install -g wct-sauce@1.6.0
-    export NPM_ROOT=$(npm root -g)
-    cp -r $NPM_ROOT/wct-sauce $NPM_ROOT/web-component-tester/node_modules
-    git clone -b ${CI_BRANCH} --single-branch https://github.com/uqlibrary/uqlibrary-elements ../uqlibrary-elements
-    chmod 755 ../uqlibrary-elements/bin/*.sh
-
-    cd ../uqlibrary-elements
-    ./bin/elements_local_tests.sh
-    cd ../uqlibrary-hours
-    ../uqlibrary-elements/bin/sauce.sh
-fi
+npm install -g web-component-tester
+echo "NPM Installed"
+bower install
+echo "Running tests"
+wct --verbose
