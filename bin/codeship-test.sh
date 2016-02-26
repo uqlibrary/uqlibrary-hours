22156ae03ad82fc89d29fa0c2585fca7a6f06526
Jan-Willem Wisgerhof <jwwisgerhof@gmail.com>
2016-02-26 13:51:16 +1000

    Reverted back to old version of WCT

--- bin/codeship-test.sh
+++ bin/codeship-test.sh
@@ -3,14 +3,13 @@
 echo "Deploying branch: ${CI_BRANCH}"
 
 # REDO WHEN POLYMER 1.0 GOES LIVE
+npm install -g web-component-tester@v3.1.7
 
 # If polymer 1.0
 if [ ${CI_BRANCH} == "polymer1.0" ]; then
 
     if [ ${PIPE_NUM} == 1 ]; then
         # Run local tests
-        echo "Installing test dependencies"
-        npm install web-component-tester -g
         echo "Bower Install"
         bower install
         echo "Running tests"
@@ -20,7 +19,6 @@ if [ ${CI_BRANCH} == "polymer1.0" ]; then
     fi
 else
     # make sure wct-sauce plugin works with old version of wct
-    npm install -g web-component-tester@v3.1.7
     npm install -g wct-sauce@1.6.0
     export NPM_ROOT=$(npm root -g)
     cp -r $NPM_ROOT/wct-sauce $NPM_ROOT/web-component-tester/node_modules
