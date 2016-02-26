08161f604f229136cf4ac2d82efcee3d3b559f17
Jan-Willem Wisgerhof <jwwisgerhof@gmail.com>
2016-02-26 15:04:46 +1000

    More debugging

--- bin/codeship-test.sh
+++ bin/codeship-test.sh
@@ -21,7 +21,8 @@ else
     # make sure wct-sauce plugin works with old version of wct
     npm install -g web-component-tester@v3.1.7
     npm install -g wct-sauce@1.6.0
-    export NPM_ROOT=$(npm root -g)
+    NPM_ROOT=$(npm root -g)
+    echo ">>>>> NPM ROOT: $NPM_ROOT"
     cp -r $NPM_ROOT/wct-sauce $NPM_ROOT/web-component-tester/node_modules
     git clone -b ${CI_BRANCH} --single-branch https://github.com/uqlibrary/uqlibrary-elements ../uqlibrary-elements
     chmod 755 ../uqlibrary-elements/bin/*.sh
