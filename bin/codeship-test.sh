0bd304d36ee7445d6e30246909eacb618e5a4a16
Jan-Willem Wisgerhof <jwwisgerhof@gmail.com>
2016-02-26 13:36:58 +1000

    Update codeship test file

--- bin/codeship-test.sh
+++ bin/codeship-test.sh
@@ -19,6 +19,13 @@ if [ ${CI_BRANCH} == "polymer1.0" ]; then
     fi
 else
     # make sure wct-sauce plugin works with old version of wct
+    npm install -g web-component-tester@v3.1.7
+    npm install -g wct-sauce@1.6.0
+    export NPM_ROOT=$(npm root -g)
+    cp -r $NPM_ROOT/wct-sauce $NPM_ROOT/web-component-tester/node_modules
+    git clone -b ${CI_BRANCH} --single-branch https://github.com/uqlibrary/uqlibrary-elements ../uqlibrary-elements
+    chmod 755 ../uqlibrary-elements/bin/*.sh
+
     cd ../uqlibrary-elements
     ./bin/elements_local_tests.sh
     cd ../uqlibrary-hours
