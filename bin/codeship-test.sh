9735dd980df1770aba165f353d74540760a5f991
Jan-Willem Wisgerhof <jwwisgerhof@gmail.com>
2016-03-24 14:47:09 +1000

    New testing setup for Polymer1.0

--- bin/codeship-test.sh
+++ bin/codeship-test.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
 
-echo "Testing branch: ${CI_BRANCH}"
+echo "Testing branch: ${CI_BRANCH} with pipe ${PIPE_NUM}"
 
-# REDO WHEN POLYMER 1.0 GOES LIVE
+if [ ${CI_BRANCH} != "GH_PAGES" ] && [ ${PIPE_NUM} == 1 ]; then
+    # Run local tests
+    echo "Installing global"
+    npm install -g bower web-component-tester
 
-# If polymer 1.0
-if [ ${CI_BRANCH} == "polymer1.0" ]; then
+    echo "Installing bower dependencies"
+    bower install
 
-    if [ ${PIPE_NUM} == 1 ]; then
-        # Run local tests
-        echo "Starting local WCT tests"
-        bower install
-        wct
-    else
-        echo "Pipe not used"
-    fi
+    echo "Starting local WCT tests"
+    wct
 else
-    # make sure wct-sauce plugin works with old version of wct
-    cd ../uqlibrary-elements
-    ./bin/elements_local_tests.sh
-    cd ../uqlibrary-hours
-    ../uqlibrary-elements/bin/sauce.sh
+    echo "Pipe not used."
 fi
