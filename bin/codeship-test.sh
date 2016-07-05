4b7dd1d2a1c3ac257f6df7e5d0ce6a6bfc4bc823
Lea de Groot <l.degroot@library.uq.edu.au>
2016-07-05 11:59:32 +1000

    update wct version  (PT 121100769)
    error reported in https://github.com/Polymer/web-component-tester/issues/332

--- bin/codeship-test.sh
+++ bin/codeship-test.sh
@@ -2,10 +2,11 @@
 
 echo "Testing branch: ${CI_BRANCH} with pipe ${PIPE_NUM}"
 
-if [ ${CI_BRANCH} != "GH_PAGES" ] && [ ${PIPE_NUM} == 1 ]; then
+# no testing for gh-pages - documentation branch
+if [ ${CI_BRANCH} != "GH_PAGES" ]; then
     # Run local tests
     echo "Installing global"
-    npm install -g bower web-component-tester
+    npm install -g bower web-component-tester@4.2.2
 
     echo "Installing bower dependencies"
     bower install
