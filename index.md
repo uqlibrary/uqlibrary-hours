0ddcebe13fc29604053f22dcfa39a6117eae1cfe
Ashley Wilson <Ashley.Wilson@uq.edu.au>
2019-08-16 14:45:17 +1000

    seed gh-pages

---
+++ index.md
@@ -0,0 +1,32 @@
+# uqlibrary-hours
+
+[![Codeship Status for uqlibrary/uqlibrary-hours](https://app.codeship.com/projects/68f81e60-8502-0132-ea60-52ba9424c583/status?branch=polymer1.0)](https://codeship.com/projects/58586)
+[![Dependency Status](https://david-dm.org/uqlibrary/uqlibrary-hours.svg)](https://david-dm.org/uqlibrary/uqlibrary-hours)
+[![Dev Dependency Status](https://david-dm.org/uqlibrary/uqlibrary-hours/dev-status.svg)](https://david-dm.org/uqlibrary/uqlibrary-hours?type=dev)
+
+uqlibrary-hours displays academic hours to the end user
+
+Full documentation and demo can be found in [GitHub Pages](https://uqlibrary.github.io/uqlibrary-hours/uqlibrary-hours/). [Demo of the compact view](https://uqlibrary.github.io/uqlibrary-hours/uqlibrary-hours/demo/index-compact.html) is available separately.
+
+## Getting Started
+
+```sh
+npm install -g bower web-component-tester polymer-cli
+npm install
+bower install
+```
+
+## Developing
+
+- Please adhere to the Polymer code style guide provided at [Style Guide](http://polymerelements.github.io/style-guide/).
+- Colors and common styles are imported (bower install) from [uqlibrary-styles](http://github.com/uqlibrary/uqlibrary-styles).
+- A preview of the component can be viewed locally by running `npm start`. Use the second URL from the command output.
+- GitHub pages should be updated after every commit to `polymer1.0` branch by running `bin/generate-gh-pages.sh`
+
+## Testing
+
+Tests are run using the Web Component Tester.
+
+```sh
+npm test
+```
---
+++ uqlibrary-hours/elements/uqlibrary-hours.js
@@ -0,0 +1,186 @@
+(function () {
+  Polymer({
+    is: 'uqlibrary-hours',
+    properties: {
+      /** Opening hours of all libraries in JSON format */
+      hours: {
+        type: Array,
+        observer: "_hoursChanged"
+      },
+      /**
+       * Autoloads the library opening hours from the API
+       * @type Object
+       */
+      autoLoad: {
+        type: Object,
+        value: true
+      },
+      /**
+       * Renders the element in compact view
+       * @type Object
+       */
+      compactView: {
+        type: Object,
+        value: false
+      },
+      /**
+       * Prefix for the google analytics category name. For example: "Home page"
+       */
+      gaCategoryPrefix: {
+        type: String,
+        value: '',
+        observer: '_gaCategoryPrefixChanged'
+      },
+      /**
+       * Holds the Google Analytics app name of this component
+       */
+      _gaAppName: {
+        type: String,
+        value: ''
+      },
+      /**
+       * Required. Whether the app should start in standalone mode or not.
+       * @type Object
+       */
+      standAlone: {
+        type: Object,
+        value: true
+      },
+      /**
+       * Header title - application name
+       */
+      headerTitle: {
+        type: String,
+        value: 'Hours'
+      }
+    },
+    ready: function () {
+      // Fetch hours
+      if (this.autoLoad) {
+        this.$.hoursApi.get();
+      }
+    },
+    // Setup event listener for Hours
+    hoursLoaded: function (e) {
+      this.setHours(e.detail);
+    },
+    /**
+     * Filter out libraries with no departments, sorts by name/abbr and sets the "hours" variable
+     * @param hours
+     */
+    setHours: function (hours) {
+      hours.locations.forEach( function(lib, index, obj) {
+        if (!lib.departments) {
+          obj.splice(index, 1);
+          return;
+        }
+        if (!lib.abbr) {
+          lib.abbr = lib.name.substring(0,15); //15 chars
+        }
+      });
+
+     // this.hours = hours.locations.filter(function(lib){return lib.departments});
+      this.hours = _.sortBy(hours.locations, (this.compactView ? 'abbr' : 'name'));
+    },
+    /**
+     * Check for other departments open 24 hours
+     *
+     * @param {array} departments
+     */
+    _has24x7: function (departments) {
+      if (departments.length === 1) {
+        return false;
+      }
+
+      //check for 24 hours study area
+      for(var i = 1; i < departments.length; i++){
+        if (departments[i].times.status === '24hours') {
+          return true;
+        }
+      }
+      return false;
+    },
+    /** Redirects the user to the selected Library page */
+    _itemTapHandler: function (e) {
+      this.$.ga.addEvent('Click', e.model.item.name);
+      window.location.href = e.model.item.url;
+    },
+    /** Adds GA event */
+    _viewHoursClicked: function (e) {
+      this.$.ga.addEvent('Click', 'View all hours');
+      window.location.href = 'https://web.library.uq.edu.au/locations-hours/opening-hours';
+    },
+    /** Parses and formats the JSON array when hours has updated */
+    _hoursChanged: function () {
+      var self = this;
+
+      this.hours.forEach(function (item) {
+        var department = item.departments[0],
+            status = department.times.status,
+            dayString = moment().format("YYYY-MM-DD"),
+            _open, _close;
+
+        item.allDay = false;
+
+        // Strip special characters from library name and foot notes
+          item.name = self._stripStr(item.name);
+          item.fn = self._stripStr(item.fn);
+
+        // If the footnote does not contain "Info:", then hide the footnote info icon
+        item.showInfo = item.fn.indexOf('Info:') >= 0;
+
+
+        if (status === 'closed') {
+
+          item.times = "Closed";
+          item.class = "closed";
+
+        } else if(status === '24hours') {
+
+          item.times  = "Open 24 hours";
+          item.class  = "all-day";
+
+        } else {
+
+          _open  = moment(dayString + ' ' + department.open);
+          _close = moment(dayString + ' ' + department.close);
+          // Format the opening text
+          item.times = department.rendered;
+          item.class = department.times.currently_open ? 'open' : 'closed';
+
+          if (self._has24x7(item.departments)) {
+            item.allDay = true;
+            item.class  = "part-all-day";
+          }
+        }
+      });
+
+      this.fire('uqlibrary-hours-loaded');
+    },
+    /**
+     * Sets the Google Analytics app name
+     * @private
+     */
+    _gaCategoryPrefixChanged: function () {
+      this._gaAppName = (this.gaCategoryPrefix ? this.gaCategoryPrefix + ' Hours' : 'Hours');
+    },
+    /**
+     * Toggles the drawer panel of the main UQL app
+     * @private
+     */
+    _toggleDrawerPanel: function () {
+      this.fire('uqlibrary-toggle-drawer');
+    },
+    /**
+     * Strip special characters from the string
+     * @private
+     * @param str
+     */
+    _stripStr: function(str) {
+      str = str.replace(/<\/?[^>]+(>|$)/g, '').replace(/\\r?\\n|\\r|\\t|\r?\n|\r|\t/g,' '); //html tags, breaklines and tabs
+      var elem = document.createElement('textarea'); //decode &amps;, &#39;,&ndash; etc.
+      elem.innerHTML = str;
+      return elem.value.replace(/[`~!@#$%^*|+=?;'"\{\}\[\]\\\/]/g, ''); //special characters
+    }
+  });
+})();
